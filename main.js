662c5dab3075acc036eec75852f99c0e657dc417
Maha-bub007 <140233343+Maha-bub007@users.noreply.github.com>
2023-12-18 18:30:56 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+let quality=[
+    {
+        body:"Addidas",
+        title:"Letest Design",
+        img:"huddi1.jpg"
+    },
+    {
+        body:"Addidas",
+        title:"Letest Design",
+        img:"huddi1.jpg"
+    },
+    {
+        body:"Addidas",
+        title:"Letest Design",
+        img:"huddi1.jpg"
+    },
+    {
+        body:"Addidas",
+        title:"Letest Design",
+        img:"huddi1.jpg"
+    },
+    {
+        body:"Addidas",
+        title:"Letest Design",
+        img:"huddi1.jpg"
+    },
+    {
+        body:"Addidas",
+        title:"Letest Design",
+        img:"huddi1.jpg"
+    },
+
+];
+let product=document.querySelector("products");
+
